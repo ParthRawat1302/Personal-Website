73c43f66089777f7cda88210305f4e4fe96ac765
ParthRawat1302 <parthrawat0987654321@gmail.com>
2025-09-13 22:06:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/personal-website/',  // add this line
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // ensures new chunks each build
+      }
+    }
+  }
 })
+
+
